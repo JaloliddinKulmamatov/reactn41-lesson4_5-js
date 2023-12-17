6d067b13aaf9ac31d0ae3e105bf222fde88422da
JaloliddinKulmamatov <itjalol2010@gmail.com>
2023-12-17 22:25:39 +0500

    first commit

---
+++ js/js.js
@@ -0,0 +1,63 @@
+//while1
+
+// let A = 37;
+// let B = 8;
+
+// while (A >= B) {
+//     A -= B;
+// }
+//  console.log(A);
+
+
+//while2
+
+// let A = 48;
+// let B = 6;
+// let count = 0;
+
+// while (A > B) {
+//     A -= B;
+//     count+= 1;
+// }
+//  console.log(count+1);
+
+
+//while4
+// let m = 37;
+// let n = 8;
+// let count = 0;
+
+// while (m > n) {
+//     m -= n;
+//     count+= 1;
+// }
+//  console.log("Butun son", count);
+
+
+
+// while (m >= n) {
+//     m -= n;
+// }
+//  console.log("Qoldiq son",m);
+
+//while5
+
+// let n = 347;
+// let c = 0;
+
+// while (n > 0) {
+//    let lastDigit = n % 10;
+//     c = c * 10 + lastDigit;
+//     n = Math.floor(n / 10);
+// }
+
+// console.log('Berilgan sonning teskarisi', c);
+
+
+
+//while6
+
+
+
+
+
